3c446ef7b5afe0848d4b87f756221753c26538ca
Anderson Nascimento <anderson.nascimento@neurotech.com.br>
2024-06-25 11:59:10 -0300

    Criando filtros de rotas com Spring Security

---
+++ src/main/java/com/gerenciador/tarefas/entity/UsuarioAutenticado.java
@@ -0,0 +1,11 @@
+package com.gerenciador.tarefas.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioAutenticado {
+    private String userName;
+    private String password;
+}
